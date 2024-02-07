ef88278ca24083d39f3b71be8f3a1d972cbf3518
NsimbaGlory-FS <112911036+NsimbaGlory-FS@users.noreply.github.com>
2024-02-07 09:10:12 -0500

    edit

--- web-exercise01/src/pages/index.js
+++ web-exercise01/src/pages/index.js
@@ -1,8 +1,10 @@
-import * as React from "react";
+import React from "react";
+import Nav from "../components/Nav";
 
 export default function Home() {
   return (
     <section>
+      <Nav />
       <div>
         <h2>My Exercise week 1.</h2>
         <h3>I develop my coding & Deploy.</h3>
