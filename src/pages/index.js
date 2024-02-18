042dc74dee3ab6423c14fc692fda7f52a3bc5a4f
NsimbaGlory-FS <112911036+NsimbaGlory-FS@users.noreply.github.com>
2024-02-18 10:49:13 -0500

    add

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 // import { Link } from "gatsby";
 import Layout from "../components/Layout";
-// import "../styles/module.css";
+// import styles from "../styles/global.css";
 
 export default function Home() {
   return (
@@ -11,7 +11,11 @@ export default function Home() {
           <h2>web deploy for Gatsby</h2>
           <h3>Develop & Deploy</h3>
           <p>web designer & web developer and deploy Article to Gatsby.</p>
-          {/* <Link className={styles.btn} to="/projects">My Portfolio Projects</Link> */}
+          <p>
+            <button onclick="document.location='https://www.gatsbyjs.com/'">
+              HTML Tutorial
+            </button>
+          </p>
         </div>
         <img
           src="/image1.webp"
