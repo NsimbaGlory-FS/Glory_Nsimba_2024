aef2897d8b60e90b1c536cb8b821c3181bb8260f
NsimbaGlory-FS <112911036+NsimbaGlory-FS@users.noreply.github.com>
2024-02-18 02:11:52 -0500

    add

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,20 @@
-import { Link } from "gatsby";
 import React from "react";
+// import { Link } from "gatsby";
 import Layout from "../components/Layout";
-import styles from "../styles/module.css";
+// import "../styles/module.css";
 
 export default function Home() {
   return (
     <Layout>
-      <section className={styles.header}>
+      <section>
         <div>
-          <h2>My Exercise week 1.</h2>
-          <h3>I develop my coding & Deploy.</h3>
-          <p>
-            Web design & web developer is better to create the different
-            application.
-          </p>
-          <Link className={styles.btn} to="/Projects">
-            Projects
-          </Link>
+          <h2>web deploy for Gatsby</h2>
+          <h3>Develop & Deploy</h3>
+          <p>web designer & web developer and deploy Article to Gatsby.</p>
+          {/* <Link className={styles.btn} to="/projects">My Portfolio Projects</Link> */}
         </div>
         <img
-          src="/Future-of-Water-iS-1401109426 (2).jpeg"
+          src="/image1.webp"
           alt="site banner"
           style={{ maxWidth: "100%" }}
         />
