9c8efa6c79394c44b09e05b103f124fb5a66f777
NsimbaGlory-FS <112911036+NsimbaGlory-FS@users.noreply.github.com>
2024-02-19 01:22:08 -0500

    add

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,22 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [movies, setMovies] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const API_BASE =
+    process.env.NODE_ENV === "development"
+      ? `http://localhost:3001`
+      : process.env.REACT_APP_BASE_URL;
+  let ignore = false;
+  useEffect(() => {
+    if (!ignore) {
+      getMovies();
+    }
+
+    return () => {
+      ignore = true;
+    };
+  }, []);
+
+  const getMovies = async () => {
+    setLoading(true);
+    try {
+      await fetch(`${API_BASE}/movies`)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log({ data });
+          setMovies(data);
+        });
+    } catch (error) {
+      setError(error.message || "Unexpected Error");
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> Movies :</h1>
+        <ul>
+          <li> Movies</li>
+        </ul>
       </header>
     </div>
   );
