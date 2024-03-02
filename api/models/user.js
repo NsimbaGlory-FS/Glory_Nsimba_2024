07a17ef3aa55dba081741fa575d63f7731b5879b
NsimbaGlory-FS <112911036+NsimbaGlory-FS@users.noreply.github.com>
2024-03-02 15:43:10 -0500

    add

--- api/models/user.js
+++ api/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt-nodejs");
 
 const validateEmail = (email) => {
   return /^\S+@\S+\.\S+$/.test(email);
@@ -21,6 +22,28 @@ const userSchema = new mongoose.Schema({
     required: true,
     default: Date.now,
   },
+});
+
+userSchema.pre("save", function (next) {
+  const user = this;
+  if (user.isNew || user.isModified("password")) {
+    // run hashing and salting
+    bcrypt.genSalt(10, (error, salt) => {
+      if (error) {
+        return next(error);
+      }
+      bcrypt.hash(user.password, salt, null, (error, hash) => {
+        if (error) {
+          return next(error);
+        }
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    //skip hashing and salting
+    next();
+  }
 });
 
 module.exports = mongoose.model("User", userSchema);
